a00828ff4af024d99a638f36f94e018bdc9e33fe
Matthew Ernst <ernst183@umn.edu>
2018-04-14 14:03:05 -0500

    Initial push, added main menu and first level scene

---
+++ menus/main_menu/TextureButton.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	self.pressed
+	set_process(true)
+	set_process_input(true)
+
+
+func _process(delta):
+	if self.pressed:
+		get_tree().change_scene("res://scenes/FirstLevel.tscn")
